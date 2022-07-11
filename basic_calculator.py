8e4ae51d9f7a3818bb4c6a049cf2b939b2dc8d7e
PepeM1 <pepemalone07@gmail.com>
2022-07-11 19:02:49 +0100

    progress

---
+++ basic_calculator.py
@@ -0,0 +1,21 @@
+print("Enter a number, operator and another number")
+num1 = float(input("what is the first number"))
+op = input("what is the operator symbol")
+num2 = float(input("what is the second number"))
+if (op=="+"):
+    print(num1+num2)
+elif (op=="-"):
+    print(num1-num2)
+elif (op=="*"):
+    print(num1*num2)
+elif (op=="/"):
+    print(num1/num2)
+elif (op=="//"):
+    print(num1//num2)
+elif (op=="%"):
+    print(num1%num2)
+elif (op=="**"):
+    print(num1**num2)
+else:
+    print("invalid operator")
+  
