f658801fa07e45ba55629835ee2d4ef3b220d962
PepeM1 <pepemalone07@gmail.com>
2022-07-23 13:02:48 +0100

    Insta bot

---
+++ Instagram Bot/Insta_bot.py
@@ -0,0 +1,6 @@
+from instabot import Bot 
+bot = Bot()
+bot.login(username = "Millies_shed",
+          password  = "Millie1990!")
+bot.upload_photo("Crafters.jpg", #not recognising my file path?
+                caption = "Join us at https://www.milliesshed.com/")
